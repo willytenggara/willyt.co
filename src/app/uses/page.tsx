1929fca536751ed3b9db3cc71d367c42b6905593
Willy Bernardus <willybernardus@gmail.com>
2024-07-03 19:57:20 +1000

    First commit

---
+++ src/app/uses/page.tsx
@@ -0,0 +1,105 @@
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function ToolsSection({
+  children,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Section>) {
+  return (
+    <Section {...props}>
+      <ul role="list" className="space-y-16">
+        {children}
+      </ul>
+    </Section>
+  )
+}
+
+function Tool({
+  title,
+  href,
+  children,
+}: {
+  title: string
+  href?: string
+  children: React.ReactNode
+}) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+export const metadata = {
+  title: 'Uses',
+  description: 'Software I use, gadgets I love, and other things I recommend.',
+}
+
+export default function Uses() {
+  return (
+    <SimpleLayout
+      title="Software I use, gadgets I love, and other things I recommend."
+      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+    >
+      <div className="space-y-20">
+        <ToolsSection title="Workstation">
+          <Tool title="13” MacBook Air, M1, 8GB RAM, (2020)">
+            I was using an Intel-based 13” MacBook Air prior to this and the
+            difference is night and day. I’ve never heard the fans turn on a
+            single time (becasue it doesn't have one), even under the incredibly
+            heavy loads I put it through with our various launch simulations.
+          </Tool>
+          <Tool title="IBM Model M SSK Industrial Keyboard">
+            They don’t make keyboards the way they used to. I buy these any time
+            I see them go up for sale and keep them in storage in case I need
+            parts or need to retire my main.
+          </Tool>
+        </ToolsSection>
+
+        <ToolsSection title="Development tools">
+          <Tool title="Visual Studio Code">
+            I don’t care if it’s missing all of the fancy IDE features everyone
+            else relies on, VS Code is still the best text editor ever made.
+          </Tool>
+          <Tool title="iTerm2">
+            I’m honestly not even sure what features I get with this that aren’t
+            just part of the macOS Terminal but it’s what I use.
+          </Tool>
+        </ToolsSection>
+
+        <ToolsSection title="Design">
+          <Tool title="Figma">
+            I started using Figma as just a design tool but now it’s become my
+            virtual whiteboard for the entire company. Never would have expected
+            the collaboration features to be the real hook.
+          </Tool>
+        </ToolsSection>
+        <ToolsSection title="Productivity">
+          <Tool title="Notion">
+            I use Notion for basically everything, from managing my personal
+            life, studies, and work.
+          </Tool>
+          <Tool title="Reflect">
+            Using a daily notes system instead of trying to keep things
+            organized by topics has been super powerful for me. And with
+            Reflect, it’s still easy for me to keep all of that stuff
+            discoverable by topic even though all of my writing happens in the
+            daily note.
+          </Tool>
+          <Tool title="SavvyCal">
+            Great tool for scheduling meetings while protecting my calendar and
+            making sure I still have lots of time for deep work during the week.
+          </Tool>
+          <Tool title="Focus">
+            Simple tool for blocking distracting websites when I need to just do
+            the work and get some momentum going.
+          </Tool>
+        </ToolsSection>
+      </div>
+    </SimpleLayout>
+  )
+}
