fda3dbe87097fd3e74351e9085be96ce8c204aa4
Willy Bernardus <willybernardus@gmail.com>
2024-12-19 16:29:34 +1100

    change email

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -11,7 +11,7 @@ export async function GET(req: Request) {
 
   let author = {
     name: 'Willy Tenggara',
-    email: 'me@willyt.co',
+    email: 'itsme@willyt.co',
   }
 
   let feed = new Feed({
