ea7725c8926a71839a642ae44d6ad26f09590b12
Willy Bernardus <willybernardus@gmail.com>
2025-01-30 00:31:51 +1100

    change photo format

--- src/lib/calculateReadDurations.ts
+++
@@ -1,7 +0,0 @@
-export const calculateReadDuration = (
-  content: string,
-  wordsPerMinute: number = 200,
-): number => {
-  const wordCount = content.split(/\s+/).length
-  return Math.ceil(wordCount / wordsPerMinute)
-}
