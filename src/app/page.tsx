95b8a5dd5ee4fa3d082f1745bb8f77e83fc3ac57
Willy Bernardus <willybernardus@gmail.com>
2025-06-17 15:42:29 +0700

    update react version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -231,10 +231,15 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      {/* <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button
+        href="/resume.pdf"
+        variant="secondary"
+        className="group mt-6 w-full"
+        download={true}
+      >
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
-      </Button> */}
+      </Button>
     </div>
   )
 }
